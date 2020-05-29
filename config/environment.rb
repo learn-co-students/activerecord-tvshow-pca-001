508c57d0525594674175b1896b6d2adf82417aa0
Aaron Wilson <adub65@gmail.com>
2020-05-29 09:03:19 -0400

    Active

--- config/environment.rb
+++ config/environment.rb
@@ -4,7 +4,7 @@ require 'active_record'
 
 Bundler.require
 
-Dir[File.join(File.dirname(__FILE__), "../app/models", "*.rb")].each {|f| require f}
+Dir[File.join(File.dirname(__FILE__), "../app", "*.rb")].each {|f| require f}
 Dir[File.join(File.dirname(__FILE__), "../lib/support", "*.rb")].each {|f| require f}
 
 DB = ActiveRecord::Base.establish_connection({
